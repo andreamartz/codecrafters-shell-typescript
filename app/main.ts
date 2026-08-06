347cca6ad2bcec3e22a7cf166e93e06f325dfbde
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2026-08-06 14:26:45 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,9 +8,15 @@ const rl = createInterface({
 
 rl.prompt();
 
-rl.on('line', (input) => {
+rl.on('line', (input: string): void => {
   if (input === 'exit') {
     rl.close();
+    return;
+  }
+  if (input.slice(0, 5) === "echo ") {
+    const textToEcho = input.slice(5);
+    console.log(`${textToEcho}`);
+    rl.prompt();
     return;
   }
   console.log(`${input}: command not found`);
